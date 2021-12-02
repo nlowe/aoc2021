dde8316b834f9c2646a38a761bed6ac1c115f618
Nathan Lowe <public@nlowe.me>
2021-12-01 21:09:26 -0800

    Tidy imports for day 2

--- challenge/day2/a.go
+++ challenge/day2/a.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/nlowe/aoc2021/util"
-
 	"github.com/spf13/cobra"
 
 	"github.com/nlowe/aoc2021/challenge"
+	"github.com/nlowe/aoc2021/util"
 )
 
 func aCommand() *cobra.Command {
